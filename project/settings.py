4eb8543dbe8581aa681f873dd19c38567b46f7f5
Ron Southwick <ronlsouthwick@gmail.com>
2023-01-17 14:45:25 -0500

    let's change the name of this dir to 'project'

--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todo.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'todo.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
