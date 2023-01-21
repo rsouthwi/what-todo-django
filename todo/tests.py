197fc3d3775eacd8378fb5140b25c7234b4bab92
Ron Southwick <ronlsouthwick@gmail.com>
2023-01-21 10:44:57 -0500

    some thoughts

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+"""
+test views:
+    - active list view
+    - list dashboard
+test api:
+    - create new list POST
+    - update list PATCH
+    - delete list DELETE
+    - add task to list POST
+    - change task state POST
+    - delete task from list DELETE
+    - change task name PUT new_name, same desc, list association, completion state
+    - change task's associated list
+"""
