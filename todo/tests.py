3038a4aa88f3e0986a3279642448c895ded0d16c
Ron Southwick <ronlsouthwick@gmail.com>
2023-01-25 14:50:05 -0500

    more tests

--- todo/tests.py
+++ todo/tests.py
@@ -129,3 +129,100 @@ class ViewTests(TestCase):
 
         # assert
         self.assertTrue(Task.objects.filter(name=expected_task_name))
+
+    def test_list_toggle_active_view__changes_state_from_true_to_false(self):
+        # arrange
+        url = reverse("list-toggle-active", kwargs={"slug": self.todo_list.slug})
+        expected_active = False
+        self.todo_list.active = not expected_active  # make sure we're not starting off with the expected value
+        self.todo_list.save()
+
+        # act
+        self.client.get(url)
+
+        # assert
+        self.assertEqual(ToDoList.objects.get(id=self.todo_list.id).active, expected_active)
+
+    def test_list_toggle_active_view__changes_state_from_false_to_true(self):
+        # arrange
+        url = reverse("list-toggle-active", kwargs={"slug": self.todo_list.slug})
+        expected_active = True
+        self.todo_list.active = not expected_active  # make sure we're not starting off with the expected value
+        self.todo_list.save()
+
+        # act
+        self.client.get(url)
+
+        # assert
+        self.assertEqual(ToDoList.objects.get(id=self.todo_list.id).active, expected_active)
+
+    def test_list_delete_view__deletes_list(self):
+        # arrange
+        url = reverse("list-delete", kwargs={"slug": self.todo_list.slug})
+
+        # act
+        self.client.post(url)
+
+        # assert
+        self.assertEqual(ToDoList.objects.filter(slug=self.todo_list.slug).count(), 0)
+
+    def test_list_update_view__updates_list(self):
+        # arrange
+        url = reverse("list-update", kwargs={"slug": self.todo_list.slug})
+        expected_new_title = "new test list new!"
+
+        # act
+        self.client.post(url, {"title": expected_new_title})
+
+        # assert
+        self.assertNotEqual(ToDoList.objects.filter(title=expected_new_title).count(), 0)
+
+    def test_list_detail__displays_list_title(self):
+        # arrange
+        url = reverse("list-detail", kwargs={"slug": self.todo_list.slug})
+
+        # act
+        response = self.client.get(url)
+
+        # assert
+        self.assertContains(response, self.todo_list.title)
+
+    def test_task_toggle_completed_view__changes_state_of_task_from_false_to_true(self):
+        # arrange
+        expected_completed_state = True
+        test_task = Task.objects.create(name="test task",
+                                        todo_list=self.todo_list,
+                                        completed=not expected_completed_state)
+        url = reverse("task-toggle-complete", kwargs={"pk": test_task.id})
+
+        # act
+        self.client.get(url)
+
+        # assert
+        self.assertEqual(Task.objects.get(id=test_task.id).completed, expected_completed_state)
+
+    def test_task_toggle_completed_view__changes_state_of_task_from_true_to_false(self):
+        # arrange
+        expected_completed_state = False
+        test_task = Task.objects.create(name="test task",
+                                        todo_list=self.todo_list,
+                                        completed=not expected_completed_state)
+        url = reverse("task-toggle-complete", kwargs={"pk": test_task.id})
+
+        # act
+        self.client.get(url)
+
+        # assert
+        self.assertEqual(Task.objects.get(id=test_task.id).completed, expected_completed_state)
+
+    def test_task_update__changes_task_description(self):
+        # arrange
+        expected_description = "this is the new description"
+        test_task = Task.objects.create(name="test task", todo_list=self.todo_list)
+        url = reverse("task-update", kwargs={"pk": test_task.id})
+
+        # act
+        self.client.post(url, {"name": test_task.name, "description": expected_description})
+
+        # asseert
+        self.assertEqual(Task.objects.get(id=test_task.id).description, expected_description)
